d43b9a9589df7c3dfdeaae060d1b1b84691f2af6
TrevorCecil <trexgaming583@gmail.com>
2024-07-18 16:20:40 +0300

    first commit

---
+++ django1/views.py
@@ -0,0 +1,47 @@
+from django.shortcuts import render,redirect
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login,logout
+from django.contrib.auth.decorators import login_required
+# Create your views here.
+def login_page(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username,password=password )
+
+            if user is not None:
+                login(request,user)
+                return redirect('index')
+            else:
+                messages.info(request,'Username or Password is incorrect')
+                return render(request, 'login.html')
+        context = {}
+        return render(request, 'login.html')
+
+def logout_page(request):
+    logout(request)
+    return redirect('login')
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+    else:
+        form = CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request,'Account was created for '+user)
+                return redirect('login')
+        context = {'form': form}
+        return render(request, 'signup.html',context)
+
+@login_required(login_url='login')
+def index(request):
+    return render(request, 'index.html')
